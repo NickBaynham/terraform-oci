cf79c1f1a0397414529c6363ce3bd9062657c8cd
Nick Baynham <nick.baynham@oracle.com>
2020-11-17 16:38:18 -0500

    Added availability domains data output

--- tf-provider/outputs.tf
+++ tf-provider/outputs.tf
@@ -1,4 +1,4 @@
 # Output the "list" of all availability domains
-output "all-availabiity-domains-in-your-tenance" {
+output "all-availability-domains-in-your-tenancy" {
   value = data.oci_identity_availability_domains.ads.availability_domains
 }
