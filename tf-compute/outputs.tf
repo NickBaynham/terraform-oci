928303afecf869fdd593b7dbf5533fbbe7247987
Nick Baynham <nick.baynham@oracle.com>
2020-11-17 18:18:29 -0500

    added outputs to compute

--- tf-compute/outputs.tf
+++ tf-compute/outputs.tf
@@ -1,3 +1,11 @@
 output "name-of-first-availability-domain" {
   value = module.availability-domains.all-availability-domains-in-your-tenancy[0].name
 }
+
+output "compute-instance-ip" {
+  value = oci_core_instance.ubuntu_instance.public_ip
+}
+
+output "compute-instance-id" {
+  value = oci_core_instance.ubuntu_instance.id
+}
