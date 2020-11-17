4a201133fc844b4d3b3b7dc8b7ac434536d59310
Nick Baynham <nick.baynham@oracle.com>
2020-11-17 17:20:31 -0500

    added compartment tf

--- tf-compartment/variables.tf
+++ tf-compartment/variables.tf
@@ -5,3 +5,14 @@ variable "private_key_path" {}
 variable "compartment_ocid" {}
 variable "region" {}
 
+variable "compartment_name" {
+  description = "The Name of the Compartment to Create"
+  type = string
+  default = "tf-compartment"
+}
+
+variable "compartment_description" {
+  description = "Description of the Compartment to be created"
+  type = string
+  default = "Terraform Managed Compartment"
+}
